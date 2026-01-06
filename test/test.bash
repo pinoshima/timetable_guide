d103067f036e8d67ad3a9a94c69b86d9c1bc8512
Kaito Shima <s24c1064nq@s.chibakoudai.jp>
2026-01-07 00:56:51 +0900

    fix

--- test/test.bash
+++ test/test.bash
@@ -2,21 +2,26 @@
 # SPDX-FileCopyrightText: 2025 Kaito Shima
 # SPDX-License-Identifier: BSD-3-Clause
 
+set -e
+
 dir=~
 [ "$1" != "" ] && dir="$1"
 
-cd $dir/ros2_ws || exit 1
-colcon build || exit 1
-source $dir/.bashrc
+cd "$dir/ros2_ws" || exit 1
 
-timeout 10 ros2 launch timetable_guide timetable.launch.py > /tmp/timetable_guide.log 2>&1
+source /opt/ros/humble/setup.bash
 
-cat /tmp/timetable_guide.log |
-grep "Location Publisher started" || exit 1
+colcon build
+source install/setup.bash
 
-cat /tmp/timetable_guide.log |
-grep "Station Selector started" || exit 1
+export RCUTILS_LOGGING_BUFFERED_STREAM=1
 
-cat /tmp/timetable_guide.log |
-grep "Nearest station" || exit 1
+timeout 10 ros2 launch timetable_guide timetable.launch.py --screen \
+  > /tmp/timetable_guide.log 2>&1
+
+cat /tmp/timetable_guide.log
+
+grep "Location Publisher started" /tmp/timetable_guide.log
+grep "Station Selector started" /tmp/timetable_guide.log
+grep "Nearest station" /tmp/timetable_guide.log
 
